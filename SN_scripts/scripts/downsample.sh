6a6d5fb119ca1a14ae58191bcf5d001fcad705eb
vaibhavad <vaibhavadlakha95@gmail.com>
2024-06-27 18:38:29 +0000

    main script

---
+++ SN_scripts/scripts/downsample.sh
@@ -0,0 +1,36 @@
+declare -A tasks_dict=(
+    [MIRACLRetrieval]="en de fr es ru ja zh fa ar fi ko id th te hi"
+    [TopiOCQA]="validation"
+    [MSMARCO-PL]="test"
+    [MSMARCO]="test"
+    [FEVER]="test"
+    [ClimateFEVER]="test"
+    [HotpotQA]="test"
+    [HotpotQA-PL]="test"
+    [DBPedia]="test"
+    [DBPedia-PL]="test"
+    [NeuCLIR2022Retrieval]="rus zho fas"
+    [NeuCLIR2023Retrieval]="rus zho fas"
+    [NQ]="test"
+    [NQ-PL]="test"
+    [RiaNewsRetrieval]="test"
+    [QuoraRetrieval]="test"
+    [Quora-PL]="test validation"
+)
+
+tasks=("${!tasks_dict[@]}")
+
+models=(
+    "intfloat/e5-large-v2"
+)
+
+for model in "${models[@]}"; do
+    for task in "${tasks[@]}"; do
+        for split in ${tasks_dict[$task]}; do
+            /home/toolkit/./eai job new -f SN_scripts/config/default.yaml --field id -- /bin/bash -c \
+            "source /opt/conda/bin/activate /home/toolkit/mteb-lite/.conda && \
+            bash run_all.sh $task $model 384 $split \
+            >> /home/toolkit/mteb-lite/$task-$model-$split.out.log 2>&1"
+        done
+    done
+done

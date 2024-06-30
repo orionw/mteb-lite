7fe3086e34ee3c006ecd56ee0b30aaa045d5892c
Orion Weller <wellerorion@gmail.com>
2024-06-30 18:16:17 -0400

    finished bm25

--- launch_all_bm25.sh
+++ launch_all_bm25.sh
@@ -15,6 +15,12 @@ do
         skipped_first=True
         continue
     fi
+
+    # if the run file already exists, skip
+    if [ -f "datasets/${dataset_name}--${split}/run_${dataset_name}--${split}.trec" ]; then
+        echo "Run file already exists, skipping..."
+        continue
+    fi
     echo "$dataset_name $lang $split $subsplit"
     bash mine_bm25.sh $dataset_name $lang $split $subsplit
 done < tasks_to_downsample.csv

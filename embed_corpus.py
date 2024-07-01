02375902ca3867da1b569ec8f19183cc1bdc3df3
vaibhavad <vaibhavadlakha95@gmail.com>
2024-07-01 19:00:36 +0000

    change to use mteb default models

--- embed_corpus.py
+++ embed_corpus.py
@@ -15,6 +15,7 @@ import torch
 from datasets import load_dataset, Dataset
 from sentence_transformers import SentenceTransformer
 import mteb
+from mteb.models.text_formatting_utils import corpus_to_texts
 
 from run_mteb_on_datasets import load_model
 
@@ -22,7 +23,12 @@ from run_mteb_on_datasets import load_model
 set_seed(123456)
 
 
-def embed_dataset(args, dataloader: list, model, use_vllm: bool = False):
+def embed_dataset(args, corpus: list[dict[str, str]] | dict[str, list[str]], model):
+    ids = []
+    docs = []
+    for k, v in corpus.items():
+        ids.append(k)
+        docs.append(v)
     # Start the multi-process pool on all available CUDA devices
 
     # save embeddings to file, at nfs/{dataset_name}/{model_name}.npz
@@ -36,42 +42,21 @@ def embed_dataset(args, dataloader: list, model, use_vllm: bool = False):
 
     if os.path.isfile(file_name):
         os.remove(file_name)
+    
+    outputs = model.encode_corpus(docs)
+
+    sentences = corpus_to_texts(docs)
 
     file_to_write = open(file_name, "w")
     total = 0
-    # NOTE: I batch this to avoid having to hold all in memory, although it's not as efficient
-    # because of this the models base batch size is high and this controls it
-    for i, batch in enumerate(tqdm(dataloader)):
-        # Compute the embeddings using the multi-process pool
-        sentences = batch["text"]
-        ids = batch["id"] if "id" in batch else batch["doc_id"]
-        if use_vllm:
-            outputs = model.encode(sentences)
-            for idx, item in enumerate(outputs):
-                # save `id` and `embeddings` for the index and `text` if we want to use it for queries
-                file_to_write.write(json.dumps({"id": ids[idx], "vector": item.outputs.embedding, "text": sentences[idx]}) + "\n")
-                total += 1
-        else:
-            batch_emb = model.encode(sentences, batch_size=args.batch_size, prompt_name=None)
-            for idx, item in enumerate(batch_emb):
-                # save `id` and `embeddings` for the index and `text` if we want to use it for queries
-                file_to_write.write(json.dumps({"id": ids[idx], "vector": item.tolist(), "text": sentences[idx]}) + "\n")
-                total += 1
+    for idx, item in enumerate(outputs):
+        # save `id` and `embeddings` for the index and `text` if we want to use it for queries
+        file_to_write.write(json.dumps({"id": ids[idx], "vector": item.tolist(), "text": sentences[idx]}) + "\n")
+        total += 1
 
     file_to_write.close()
-    if use_vllm:
-        dim_size = outputs.outputs.embedding.size(0)
-    else:
-        dim_size = batch_emb[0].shape[0]
+    dim_size = outputs.shape[1]
     print(f"Embedding dimension: {dim_size}")
-
-
-
-def combine_doc(doc: dict):
-    if "title" in doc:
-        return doc["title"] + " " + doc["text"]
-    else:
-        return doc["text"]
 
 
 if __name__ == "__main__":
@@ -91,19 +76,7 @@ if __name__ == "__main__":
     if args.subsplit:
         corpus = corpus[args.subsplit]
 
-    input = [combine_doc(item) for item in corpus.values()]
-    ids = list(corpus.keys())
-    print(f"Dataset {args.dataset} has {len(input)} documents")
-
-    # Load the model
-    # if "mistral" in args.model:
-    #     use_vllm = True
-    #     model = LLM(model=args.model, enforce_eager=True, dtype="auto", disable_sliding_window=True)
-    # else:
-    use_vllm = False
-    model = load_model(args.model, args.dataset)
-
-    dataloader = DataLoader(Dataset.from_dict({"text": input, "id": ids}), batch_size=args.batch_size, num_workers=0)
+    model = mteb.get_model(args.model)
 
     # Embed the dataset
-    embed_dataset(args, dataloader, model, use_vllm=use_vllm)
+    embed_dataset(args, corpus, model)

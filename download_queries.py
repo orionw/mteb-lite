c5dd5b2ce77cc2bf90686c487bd34dd6ce73a090
Orion Weller <wellerorion@gmail.com>
2024-07-22 13:26:04 -0400

    update instructions

--- download_queries.py
+++ download_queries.py
@@ -2,8 +2,10 @@ from datasets import load_dataset
 import argparse
 import os
 import mteb
+from mteb.models.instructions import task_to_instruction
 
-def download(dataset_name: str, split: str):
+def download(dataset_name: str, split: str, use_instructions: bool = False):
+    use_instructions = True # TODO: remove this when we're done with 7B models
     tasks = mteb.get_tasks(tasks=[dataset_name])
     assert len(tasks) == 1, f"Expected 1 task, got {len(tasks)}"
     tasks[0].load_data()
@@ -12,16 +14,27 @@ def download(dataset_name: str, split: str):
     output_path = f"artifacts/{dataset_name.replace('/', '--')}-{split}.tsv"
     print(f"Dataset {dataset_name} has {len(queries)} queries")
 
+    if use_instructions:
+        instruction = task_to_instruction(dataset_name, is_query=True)
+    else:
+        instruction = None
+
     # save the dataset out as a tsv file
     os.makedirs("artifacts", exist_ok=True)
     with open(output_path, "w") as f:
         for query_id, query in queries.items():
-            f.write(f"{query_id}\t{query}\n")
+            if instruction is not None:
+                # NOTE: this is e5-mistral-instruct specific
+                new_query =  f'Instruct: {instruction}\\nQuery: {query}'
+                f.write(f"{query_id}\t{new_query}\n")
+            else:
+                f.write(f"{query_id}\t{query}\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset_name", type=str, help="The name of the dataset to download")
     parser.add_argument("-s", "--split", type=str, default="test", help="The split to download")
+    parser.add_argument("-i", "--use_instructions", action="store_true", help="Whether to use instructions")
     args = parser.parse_args()
     download(args.dataset_name, args.split)
